d76ff19cddecfe1af4081a898296e244f86d7218
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2026-09-20 20:01:08 +0700

    update4

--- test_login.py
+++ test_login.py
@@ -4,6 +4,10 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    print(page.title())
+    
 
+    page.fill("input[name='username']", "Admin")
+    page.fill("input[name='password']", "admin123")
+    page.click("button[type='submit']")
+    assert page.to_be_visible("text=Dashboard")
     browser.close()
